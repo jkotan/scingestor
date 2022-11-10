c9494b159ceac134d7bafae76c5006514d85e73d
Jan Kotanski <jankotan@gmail.com>
2022-11-10 12:03:09 +0100

    update format_default.sh parameters

--- config/format_default.sh
+++ config/format_default.sh
@@ -1,14 +1,24 @@
 #!/bin/env bash
 
 # read parameters from the command-line
-if [[ ! -z $1 ]]; then
-    export SCICAT_URL="$1"
+if [[ -z $2 ]] ; then
+    echo "usage: format_default.sh  <INPUT_FILE>  <OUTPUT_FILE> [ <SCICAT_URL> <BEAMTIME_DIR> <INGESTOR_VAR_DIR>]"
+    exit 1
+else
+    # template_default.yaml
+    export INFILE="$1"
+    # default.yaml
+    export OUTFILE="$2"
 fi
-if [[ ! -z $2 ]]; then
-    export BEAMTIME_DIR="$2"
-fi
+
 if [[ ! -z $3 ]]; then
-    export INGESTOR_VAR_DIR="$3"
+    export SCICAT_URL="$3"
+fi
+if [[ ! -z $4 ]]; then
+    export BEAMTIME_DIR="$4"
+fi
+if [[ ! -z $5 ]]; then
+    export INGESTOR_VAR_DIR="$5"
 fi
 
 # default parameters
@@ -22,12 +32,12 @@ if [[ -z $INGESTOR_VAR_DIR ]]; then
     export INGESTOR_VAR_DIR="/gpfs/current/scratch_bl/scingestor"
 fi
 
-rm -f default.yaml default.yaml.tmp
+rm -f ${OUTFILE} ${OUTFILE}.tmp
 
 ( echo "cat <<EOF >default.yaml";
-  cat default_template.yaml;
+  cat ${INFILE};
   echo "EOF";
-) >default.yaml.tmp
-. default.yaml.tmp
+) >${OUTFILE}.tmp
+. ${OUTFILE}.tmp
 
-rm -f default.yaml.tmp
+rm -f ${OUTFILE}.tmp
