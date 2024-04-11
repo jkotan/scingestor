5144a5192bf22ad5783da77f1825c3ed6b145342
Jan Kotanski <jankotan@gmail.com>
2024-04-11 19:41:30 +0200

    Issue 283: add ubuntu noble tests (#284)
    * add noble tests
    
    * fix typo
    
    * add missing files
    
    * fix typo
    
    * update classifiers

--- setup.py
+++ setup.py
@@ -113,6 +113,8 @@ SETUPDATA = dict(
         'Programming Language :: Python :: 3.8',
         'Programming Language :: Python :: 3.9',
         'Programming Language :: Python :: 3.10',
+        'Programming Language :: Python :: 3.11',
+        'Programming Language :: Python :: 3.12',
     ],
     packages=["scingestor"],
     scripts=[
